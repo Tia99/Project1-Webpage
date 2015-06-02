4575efd737e88a988206205538e5260a14bf6e4f
Tia99 <tiafairy.me@gmail.com>
2015-06-01 22:33:02 -0700

    Simple dynamic page

--- mywebapp/mywebapp.py
+++ mywebapp/mywebapp.py
@@ -2,8 +2,9 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        name = "John"
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('<html><head><title>World</title></head><body><h1>Hello '+ name +'!</h1></body></html>')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
