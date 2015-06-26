67c46ca065f3b921e6f815093bc65ae8238def81
Tia99 <tiafairy.me@gmail.com>
2015-06-25 21:57:01 -0700

    Added validation

--- mywebapp/mywebapp.py
+++ mywebapp/mywebapp.py
@@ -15,13 +15,38 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('main.html')
         self.response.write(template.render(template_values))
 
-class CommentHandler(webapp2.RequestHandler):
     def post(self):
-        user_name = self.request.get("username")
-        comment = self.request.get("comment")
-        self.redirect("/")
+        username_entered = self.request.get('username')
+        comment_entered = self.request.get('comment')
+        if self.valid_username(username_entered) and self.valid_comment(comment_entered):
+            #TODO: save to db
+            self.redirect('/')
+        else:
+            template_values = {
+                'error': 'Username or comment are not valid!',
+                'username': username_entered,
+                'comment': comment_entered
+            }
+            template = JINJA_ENVIRONMENT.get_template('main.html')
+            self.response.write(template.render(template_values))
+
+    def valid_username(self, s):
+        m = len(s)
+        if m > 1 and m < 15:
+            return True
+        else:
+            return False
+
+    def valid_comment(self, s):
+        m = len(s)
+        if m > 5 and m < 1000:
+            return True
+        else:
+            return False
+
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/comment', CommentHandler)
+    ('/', MainPage),    
 ], debug=True)
